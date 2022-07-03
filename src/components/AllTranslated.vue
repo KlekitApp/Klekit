bd67b2197f28358c0653d4af148a2daf63f58132
Anton Nosenko <nos.anton@gmail.com>
2022-07-04 01:41:26 +0200

    add confetti

--- src/components/AllTranslated.vue
+++ src/components/AllTranslated.vue
@@ -1,17 +1,43 @@
 <template>
     <q-card class="q-pa-md" flat>
       <q-card-section>
-        <div class="text-h2">
+        <div class="text-h2 text-center">
             Hooray!!!
         </div>
-
-        <div class="text-h4">
+      </q-card-section>
+      <q-card-section>
+        <div class="text-h4 text-center">
             This file has full translation!!!
         </div>
-
-        <div class="text-caption">
+      </q-card-section>
+      <q-card-section>
+        <div class="text-grey text-caption text-center">
           If you want to change some key in this file - just click on "key" icon in top right corner and choose key you want to change.
         </div>
       </q-card-section>
     </q-card>
 </template>
+
+<script>
+export default {
+  mounted() {
+    this.$confetti.start({  
+        particles: [
+      {
+        type: 'rect',
+      },
+      {
+        type: 'circle',
+      },
+    ],
+    particlesPerFrame: 0.25,
+    defaultDropRate: 4,
+    defaultSize: 10,
+    windSpeedMax: 0
+  });
+  },
+  unmounted() {
+    this.$confetti.stop();
+  },
+}
+</script>
